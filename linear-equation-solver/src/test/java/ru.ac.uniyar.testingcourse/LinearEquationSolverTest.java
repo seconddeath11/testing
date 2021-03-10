24ef0a30657d15dcf4355c5feab19016cdc8f282
diana <novikova_diana2000@mail.ru>
2021-03-10 10:18:41 +0300

    assertThat

--- linear-equation-solver/src/test/java/ru.ac.uniyar.testingcourse/LinearEquationSolverTest.java
+++ linear-equation-solver/src/test/java/ru.ac.uniyar.testingcourse/LinearEquationSolverTest.java
@@ -14,5 +14,11 @@ public class LinearEquationSolverTest {
         assertEquals(-2.5, LinearEquationSolver.solve(-3.5, 8.75));
     }
     @Test
+    void precisionOldTo(){assertEquals(0.33, LinearEquationSolver.solve(3, 1), 0.5);}
+    @Test
     void equalTo(){assertThat(LinearEquationSolver.solve(-3.5, 8.75)).isEqualTo(-2.5);}
+    @Test
+    void precisionTest(){assertThat(LinearEquationSolver.solve(3, 1)).isCloseTo(0.3, within(0.1));}
+    @Test
+    void precision2Test(){assertThat(LinearEquationSolver.solve(3, 1)).isEqualTo(0.3, withPrecision(2d));}
 }
